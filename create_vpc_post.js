78845ce2b8cafa51ffd981791255bd2c2e2bd143
mondayly <51398295+mondayly@users.noreply.github.com>
2020-10-10 19:47:37 +0800

    Update create_vpc_post.js

--- create_vpc_post.js
+++ create_vpc_post.js
@@ -1,5 +1,9 @@
 #使用Postman中的body并使用raw切换成json
 {
-    "LLTD":"monday"
+    "LLTD":"monday",
     #monday can be changed
+    "LLTD2":"20.0.1.0/24",
+    "LLTD3":"20.0.2.0/24",
+    "LLTD4":"20.0.3.0/24",
+    "LLTD5":"20.0.4.0/24"
 }
